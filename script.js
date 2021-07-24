d34b13c3341c00e14529a4f42b618ba483c72fda
Deepanshu Sharma <sdeepanshu368@gmail.com>
2021-07-24 14:40:51 +0530

    The sound icon code has been modified. It is working fine now.

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-var audio = document.createElement('audio');
-audio.setAttribute('src', 'sound.mp3');
-function oncl(){
-    if(audio.loop==true){
-        audio.loop=false;
-        audio.pause;
-        console.log("stopped");
-    }
-    else if(audio.loop==false){
-        audio.loop=true;
-        audio.play();
-        console.log("started");
-    }
-}
+var myAudio = document.getElementById("myAudio");
+function togglePlay() {
+  return myAudio.paused ? myAudio.play() : myAudio.pause();
+};
